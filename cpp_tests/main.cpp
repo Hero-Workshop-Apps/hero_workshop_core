6338f249696062b406ca94e1006a1f069c685a46
Jacob Kiesel <kieseljake@gmail.com>
2020-12-30 02:03:05 -0700

    End main in cpp by returning zero correctly

--- cpp_tests/main.cpp
+++ cpp_tests/main.cpp
@@ -4,4 +4,5 @@
 int main() {
     auto response = protobuf_entry_point(::rust::Vec<uint8_t>());
     std::cout << "Got core response: " << std::string((const char*) response.data(), response.size()) << std::endl;
+    return 0;
 }
